0502e41dfcc36f043a5615afbcb4ad39cd6611ef
Giovanni Mansueto <giovanni.mansueto@dcg.com.br>
2021-10-12 17:11:41 -0300

    remove husky

--- prepare.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable global-require */
-const isCi = process.env.CI !== undefined;
-
-if (!isCi) {
-  require('husky').install();
-}
