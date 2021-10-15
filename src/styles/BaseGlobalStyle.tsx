ce4cd30f573d1546dfa22572325abb473d286a3b
Giovanni Mansueto <giovanni.mansueto@dcg.com.br>
2021-10-15 11:38:08 -0300

    create static webapp config file

--- src/styles/BaseGlobalStyle.tsx
+++ src/styles/BaseGlobalStyle.tsx
@@ -7,9 +7,11 @@ const BaseGlobalStyle: React.FC = () => {
   const router = useRouter();
 
   const { tenant } = router.query;
+  const tenanrCssUri = `https://linxcheckoutdevstorage.blob.core.windows.net/nx-ds-files/css/${tenant}/variables.css`
 
   return <>
     <link href={BASE_CSS_VARS_URI} rel="stylesheet" as="style" />
+	<link href={tenanrCssUri} rel="stylesheet" as="style" />
   </>;
 };
 
